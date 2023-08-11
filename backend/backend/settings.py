0385dfcc6c8ddc408c9e7bff7eaa676b11bac03e
Stein-NN <87211113+Stein-NN@users.noreply.github.com>
2023-08-11 10:27:02 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.21.9', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yakittygram.ddnsking.com']
 
 
 # Application definition
